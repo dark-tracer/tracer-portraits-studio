1c3f90601277e54ffd6149618d6c5daeac02c79e
Bernie Amponsah <79414278+dark-tracer@users.noreply.github.com>
2026-08-17 21:44:58 +0000

    Create entry-server.tsx

---
+++ src/entry-server.tsx
@@ -0,0 +1,7 @@
+import { createStartHandler } from '@tanstack/react-start/server';
+import { createRouter } from './router';
+
+export default createStartHandler({
+  createRouter,
+  getRouter: () => createRouter(),
+});
